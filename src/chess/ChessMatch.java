26b838852020e406b31d2df0bae4bcecc4c42887
Bruno de Fraga <brunofragaa97@gmail.com>
2020-08-03 00:13:54 -0300

    Colocando as peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 			//-------------PARTIDA DE TABULEIRO-----------------
 			//ESTÁ É A CLASSE QUE CONTÉM TODAS AS REGRAS DO JOGO
@@ -11,6 +14,7 @@ public class ChessMatch {
 			//É IMPORTANTE ENTENDER, ESTÁ CLASSE É QUEM DEVE SABER A DIMENÇÃO DO JOGO
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 			
@@ -27,5 +31,11 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK),new Position(0, 4));
+		board.placePiece(new King(board, Color.BLACK),new Position(7, 4));
 	}
 }
