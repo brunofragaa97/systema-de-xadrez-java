bc8562d655c1c443401bcf8d45a7fd50721d462a
Bruno de Fraga <brunofragaa97@gmail.com>
2020-08-04 09:50:03 -0300

    Implementando os movimentos da TORRE

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -28,6 +28,11 @@ public class Position {
 		this.column = column;
 	}
 	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
 				//DESCRIÇÃO DA CLASSE
 	@Override
 	public String toString() {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 		//AO UTILIZAR "HERANÇA", O COMPILADOR RECLAMA POIS EXIGE "CONSTRUTOR" DA CLASSE "PIECE"
 public abstract class ChessPiece extends Piece {
@@ -17,6 +18,10 @@ public abstract class ChessPiece extends Piece {
 			//APENAS USO DO GET, PARA QUE A COR DE UMA PEÇA NÃO SEJA ALTERADA, APENAS SERA ACESSADA
 	public Color getColor() {
 		return color;
+	}
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
 	}
 	
 	
